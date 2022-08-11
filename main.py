ac11d07aaf2ce625da09d9b752dc721851b523e6
Kranc77 <andrzejkrawiec77@o2.pl>
2022-08-11 13:14:19 +0200

    Pierwszy commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Projekt dla Gminy w ramach praktyk ze studiów
+# Projekt zakłada, że słupy nad przejściami dla pieszych w gminie będą wysyłały informacje o prędkości pojazdów do bazy
+# w której to algorytm będzie analizował dane pod względem wyszukania z nich możliwych anomali - tzn. piratów drogowych
+# których to pomimo informowania ich o zbyt dużej prędkości przed przejściem dla pieszych nie zwalniają
+
+# generowanie losowych danych na potrzeby stworzenia programu
+
+
+# stworzenie słownika w którym będą zapisane dane z poszczególnych miejscowości należących do gminy
+dict = {'Wola Raniżowska': [],
+        'Raniżów':[],
+        'Staniszewskie':[],
+        'Zielonka':[],
+        'Mazury':[],
+        'Korczowiska':[],
+        'Posuchy':[],
+        'Poręby Wolskie':[]
+        }
+
+
+
+
